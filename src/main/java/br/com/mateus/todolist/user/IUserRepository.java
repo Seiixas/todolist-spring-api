6dc7c2d2d17e5030e9c0ee4cce83b076676699ba
Mateus Seixas <mateuseixas@icloud.com>
2023-10-14 22:59:23 -0300

    Aula 02 - Integração com Banco de Dados

---
+++ src/main/java/br/com/mateus/todolist/user/IUserRepository.java
@@ -0,0 +1,11 @@
+package br.com.mateus.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+  public Optional<UserModel> findByUsername(String username);
+}
