cb48163489a62e3361f39f8b8b29cf68b2689537
Mateus Seixas <mateuseixas@icloud.com>
2023-10-15 01:14:22 -0300

    Aula 04 - Atualizando tarefas e validação de rotas

--- src/main/java/br/com/mateus/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/mateus/todolist/filter/FilterTaskAuth.java
@@ -4,19 +4,17 @@ import java.io.IOException;
 import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
 import br.com.mateus.todolist.user.IUserRepository;
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Componen
+@Component
 public class FilterTaskAuth extends OncePerRequestFilter {
 
   @Autowired
@@ -26,28 +24,35 @@ public class FilterTaskAuth extends OncePerRequestFilter {
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
 
-    var authorization = request.getHeader("Authorization");
+    var servletPath = request.getServletPath();
 
-    var authEncoded = authorization.substring("Basic".length()).trim();
-    var authDecoded = Base64.getDecoder().decode(authEncoded);
+    if (servletPath.startsWith("/tasks/")) {
+      var authorization = request.getHeader("Authorization");
 
-    var auth = new String(authDecoded);
+      var authEncoded = authorization.substring("Basic".length()).trim();
+      var authDecoded = Base64.getDecoder().decode(authEncoded);
 
-    var credentials = auth.split(":");
-    var username = credentials[0];
-    var password = credentials[1];
+      var auth = new String(authDecoded);
 
-    var user = this.userRepository.findByUsername(username);
+      var credentials = auth.split(":");
+      var username = credentials[0];
+      var password = credentials[1];
 
-    if (user.isEmpty()) {
-      response.sendError(401);
-    } else {
-      var passwordMatches = BCrypt.verifyer().verify(password.toCharArray(), user.get().getPassword());
-      if (passwordMatches.verified) {
-        filterChain.doFilter(request, response);
-      } else {
+      var user = this.userRepository.findByUsername(username);
+
+      if (user.isEmpty()) {
         response.sendError(401);
+      } else {
+        var passwordMatches = BCrypt.verifyer().verify(password.toCharArray(), user.get().getPassword());
+        if (passwordMatches.verified) {
+          request.setAttribute("idUser", user.get().getId());
+          filterChain.doFilter(request, response);
+        } else {
+          response.sendError(401);
+        }
       }
+    } else {
+      filterChain.doFilter(request, response);
     }
   }
 }
--- src/main/java/br/com/mateus/todolist/task/TaskController.java
+++ src/main/java/br/com/mateus/todolist/task/TaskController.java
@@ -1,10 +1,22 @@
 package br.com.mateus.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.mateus.todolist.utils.Utils;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,9 +26,37 @@ public class TaskController {
   private ITaskRepository taskRepository;
 
   @PostMapping("/")
-  public TaskModel create(@RequestBody TaskModel taskModel) {
+  public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest httpServletRequest) {
+    var idUser = httpServletRequest.getAttribute("idUser");
+    taskModel.setIdUser((UUID) idUser);
+
+    var currentDate = LocalDateTime.now();
+    if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("A data de início / data de término deve ser maior que a data atual.");
+    }
+    if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("A data de início deve ser maior que a data de término.");
+    }
+
     var task = this.taskRepository.save(taskModel);
 
-    return task;
+    return ResponseEntity.status(HttpStatus.CREATED).body(task);
+  }
+
+  @GetMapping("/")
+  public List<TaskModel> list(HttpServletRequest request) {
+    var idUser = request.getAttribute("id");
+    return this.taskRepository.findByIdUser((UUID) idUser);
+  }
+
+  @PutMapping("/{taskId}")
+  public void update(@RequestBody TaskModel taskModel, @PathVariable UUID taskId, HttpServletRequest request) {
+    var task = this.taskRepository.findById(taskId).orElse(null);
+
+    Utils.copyNonNullProperties(taskModel, task);
+
+    this.taskRepository.save(task);
   }
 }
